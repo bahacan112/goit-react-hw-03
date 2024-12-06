2dcd513c0e450aaf2ef858cafb06abb7a9d845a6
bahacan112 <bzenbil19@gmail.com>
2024-12-06 17:56:23 +0300

    iconlar eklendi

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,19 @@
 import PropTypes from "prop-types";
 import Styles from "./Contact.module.css";
+import { RiContactsBookFill } from "react-icons/ri";
+import { FaPhone } from "react-icons/fa";
+
 const Contact = ({ id, name, number, onDeleteContact }) => {
   return (
     <div className={Styles.contact}>
-      <p>{name}</p>
-      <p>{number}</p>
+      <p className={Styles.p}>
+        <RiContactsBookFill className={Styles.icon} />
+        {name}
+      </p>
+      <p className={Styles.p}>
+        <FaPhone className={Styles.icon} />
+        {number}
+      </p>
       <button onClick={() => onDeleteContact(id)}>Delete</button>
     </div>
   );
